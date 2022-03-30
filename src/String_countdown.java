39e94d8cd9e13132c53b929cfe611dbcc2f817b9
Marina-Mironova <kenderrisha1@gmail.com>
2022-03-30 11:21:19 +0200

    test main

---
+++ src/String_countdown.java
@@ -0,0 +1,13 @@
+public class String_countdown {
+    public static void main(String[] args) {
+        System.out.println(getReverseString("hello java"));
+    }
+
+    public static String getReverseString(String str){
+        String result="";
+        for(int i=(str.length()-1);i>=0; i--){
+            result=result+str.charAt(i);
+        }
+        return result;
+    }
+}
